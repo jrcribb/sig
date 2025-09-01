e69c261033f237bd7ed6d3970af18d051b0c48d7
ynqa <un.pensiero.vano@gmail.com>
2025-09-02 05:31:51 +0900

    chore: remove keymap for sig the same as archived

--- src/sig.rs
+++ src/sig.rs
@@ -7,13 +7,110 @@ use tokio::{
 };
 
 use promkit_core::{
-    crossterm::{self, event, style::ContentStyle},
+    crossterm::{self, event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyEventState, KeyModifiers}, style::ContentStyle},
     PaneFactory,
 };
 use promkit_widgets::text_editor;
 
-mod keymap;
 use crate::{highlight::highlight, spawn, terminal::Terminal, Signal};
+
+// Evaluate a key event and return the corresponding Signal.
+fn evaluate_event(
+    event: &Event,
+    state: &mut text_editor::State,
+    cmd: Option<String>,
+) -> anyhow::Result<Signal> {
+    match event {
+        Event::Key(KeyEvent {
+            code: KeyCode::Char('f'),
+            modifiers: KeyModifiers::CONTROL,
+            kind: KeyEventKind::Press,
+            state: KeyEventState::NONE,
+        }) => return Ok(Signal::GotoArchived),
+
+        Event::Key(KeyEvent {
+            code: KeyCode::Char('r'),
+            modifiers: KeyModifiers::CONTROL,
+            kind: KeyEventKind::Press,
+            state: KeyEventState::NONE,
+        }) => {
+            if cmd.is_some() {
+                return Ok(Signal::GotoStreaming);
+            }
+        }
+
+        Event::Key(KeyEvent {
+            code: KeyCode::Char('c'),
+            modifiers: KeyModifiers::CONTROL,
+            kind: KeyEventKind::Press,
+            state: KeyEventState::NONE,
+        }) => return Err(anyhow::anyhow!("ctrl+c")),
+
+        // Move cursor.
+        Event::Key(KeyEvent {
+            code: KeyCode::Left,
+            modifiers: KeyModifiers::NONE,
+            kind: KeyEventKind::Press,
+            state: KeyEventState::NONE,
+        }) => {
+            state.texteditor.backward();
+        }
+        Event::Key(KeyEvent {
+            code: KeyCode::Right,
+            modifiers: KeyModifiers::NONE,
+            kind: KeyEventKind::Press,
+            state: KeyEventState::NONE,
+        }) => {
+            state.texteditor.forward();
+        }
+        Event::Key(KeyEvent {
+            code: KeyCode::Char('a'),
+            modifiers: KeyModifiers::CONTROL,
+            kind: KeyEventKind::Press,
+            state: KeyEventState::NONE,
+        }) => state.texteditor.move_to_head(),
+        Event::Key(KeyEvent {
+            code: KeyCode::Char('e'),
+            modifiers: KeyModifiers::CONTROL,
+            kind: KeyEventKind::Press,
+            state: KeyEventState::NONE,
+        }) => state.texteditor.move_to_tail(),
+
+        // Erase char(s).
+        Event::Key(KeyEvent {
+            code: KeyCode::Backspace,
+            modifiers: KeyModifiers::NONE,
+            kind: KeyEventKind::Press,
+            state: KeyEventState::NONE,
+        }) => state.texteditor.erase(),
+        Event::Key(KeyEvent {
+            code: KeyCode::Char('u'),
+            modifiers: KeyModifiers::CONTROL,
+            kind: KeyEventKind::Press,
+            state: KeyEventState::NONE,
+        }) => state.texteditor.erase_all(),
+
+        // Input char.
+        Event::Key(KeyEvent {
+            code: KeyCode::Char(ch),
+            modifiers: KeyModifiers::NONE,
+            kind: KeyEventKind::Press,
+            state: KeyEventState::NONE,
+        })
+        | Event::Key(KeyEvent {
+            code: KeyCode::Char(ch),
+            modifiers: KeyModifiers::SHIFT,
+            kind: KeyEventKind::Press,
+            state: KeyEventState::NONE,
+        }) => match state.edit_mode {
+            text_editor::Mode::Insert => state.texteditor.insert(*ch),
+            text_editor::Mode::Overwrite => state.texteditor.overwrite(*ch),
+        },
+
+        _ => (),
+    }
+    Ok(Signal::Continue)
+}
 
 pub async fn run(
     text_editor: text_editor::State,
@@ -84,7 +181,7 @@ pub async fn run(
     loop {
         let event = event::read()?;
         let mut text_editor = shared_text_editor.write().await;
-        signal = keymap::default(&event, &mut text_editor, cmd.clone())?;
+        signal = evaluate_event(&event, &mut text_editor, cmd.clone())?;
         if signal == Signal::GotoArchived || signal == Signal::GotoStreaming {
             break;
         }
